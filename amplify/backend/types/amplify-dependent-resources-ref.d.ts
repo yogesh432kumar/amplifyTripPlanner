eab1314b6d949a7b1fc74b1fc474f8e7a31a995f
Yogesh Kumar <yogeshkumar@Yogesh-M3-Max.attlocal.net>
2024-01-15 18:13:57 -0800

    first commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifytripsplanner": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "amplifytripsplanner7c5f79c3": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "tripcontent": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
